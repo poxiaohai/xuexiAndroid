e32fa96384a326a27e9d121651c481f9ae3c69f0
cheng <cheng@haha.local>
2018-05-25 11:58:31 +0800

    voelly 代码

--- app/src/main/java/com/example/cheng/android/Volley/VolleyActivity.java
+++ app/src/main/java/com/example/cheng/android/Volley/VolleyActivity.java
@@ -1,14 +1,17 @@
 package com.example.cheng.android.Volley;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -28,7 +31,10 @@ public class VolleyActivity extends AppCompatActivity {
 
     @BindView(R.id.stringTv)
     TextView stringTv;
+    @BindView(R.id.image1)
+    ImageView imageView;
     RequestQueue mQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +43,7 @@ public class VolleyActivity extends AppCompatActivity {
         mQueue= Volley.newRequestQueue(getApplicationContext());
     }
 
-    @OnClick({R.id.stringTv,R.id.JsonObjectRequest})
+    @OnClick({R.id.stringTv,R.id.JsonObjectRequest,R.id.loadimage})
     public void onViewClicked(View view) {
         switch (view.getId()){
             case R.id.stringTv:
@@ -47,7 +53,28 @@ public class VolleyActivity extends AppCompatActivity {
                 JsonRequest();
                 break;
 
+            case R.id.loadimage:
+                loadImage();
+                break;
+
         }
+    }
+
+    private void loadImage() {
+        ImageRequest imageRequest=new ImageRequest("https://img.zcool.cn/community/0174295541fe180000019ae91f2478.jpg@2o.jpg", new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                imageView.setImageBitmap(response);
+
+            }
+        }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                imageView.setImageResource(R.mipmap.ic_launcher);
+            }
+        });
+
+        mQueue.add(imageRequest);
     }
 
     private void JsonRequest() {
