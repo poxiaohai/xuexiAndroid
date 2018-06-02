1146201d6275bc0a997d4ba7fc9d9977e9171f0a
cheng <cheng@haha.local>
2018-06-02 15:20:53 +0800

    子线程之前通信

--- app/src/main/java/com/example/cheng/android/Handler/SubThreadActivity.java
+++ app/src/main/java/com/example/cheng/android/Handler/SubThreadActivity.java
@@ -12,19 +12,18 @@ import com.example.cheng.android.R;
 import com.socks.library.KLog;
 
 public class SubThreadActivity extends AppCompatActivity {
-
+    private Handler handler1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_thread);
         Thread1 thread1=new Thread1();
         thread1.start();
-        Thread2 thread2=new Thread2(thread1.getHandler());
+        Thread2 thread2=new Thread2();
         thread2.start();
     }
 
     class Thread1 extends Thread{
-        private Handler handler1;
         public Handler getHandler(){//注意哦，在run执行之前，返回的是null
             return handler1;
         }
@@ -45,10 +44,6 @@ public class SubThreadActivity extends AppCompatActivity {
 
 
     public class Thread2 extends Thread{
-        private Handler handler2;
-        public  Thread2(Handler handler1){
-            this.handler2=handler1;
-        }
 
         @Override
         public void run() {
@@ -57,7 +52,7 @@ public class SubThreadActivity extends AppCompatActivity {
                 Message msg = Message.obtain();
                 msg.what = 1;
                 msg.obj = System.currentTimeMillis()+"";
-                handler2.sendMessage(msg);
+                handler1.sendMessage(msg);
                 KLog.e("MThread", Thread.currentThread().getName()+"----发送了消息！"+msg.obj);
                 SystemClock.sleep(1000);
             }
