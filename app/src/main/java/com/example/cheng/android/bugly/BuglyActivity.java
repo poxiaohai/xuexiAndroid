3ffcb8493a5b5562f685e04117d71bee8c7a95ce
cheng <cheng@haha.local>
2018-06-19 10:27:28 +0800

    bugly 修复图片bug

--- app/src/main/java/com/example/cheng/android/bugly/BuglyActivity.java
+++ app/src/main/java/com/example/cheng/android/bugly/BuglyActivity.java
@@ -1,17 +1,26 @@
-
 package com.example.cheng.android.bugly;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import com.example.cheng.android.BaseActivity;
 import com.example.cheng.android.R;
 
-public class BuglyActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BuglyActivity extends BaseActivity {
+
+    @BindView(R.id.image)
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bugly);
+        ButterKnife.bind(this);
+        image.setImageResource(R.drawable.img02);
 //        int count=10/0;
     }
 }
